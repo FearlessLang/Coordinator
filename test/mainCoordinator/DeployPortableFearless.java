33ccf906177c5080c20b18304b8d9a1084269f68
Marco Servetto <marco.servetto@gmail.com>
2026-01-31 18:56:30 +1300

    Command line building

--- test/mainCoordinator/DeployPortableFearless.java
+++ test/mainCoordinator/DeployPortableFearless.java
@@ -1,6 +1,9 @@
 package mainCoordinator;
 
 import tools.PortableApp;
+//This can easily be run from command line as follow:
+//go in the parent directory (Coordinator/test)
+//yourJava --class-path "youPathToCommons/Commons/Commons.jar" mainCoordinator/DeployPortableFearless.java
 
 public class DeployPortableFearless{
   public static void main(String[] a){
